abe9e23429bbec3651466efa5e7a51469d6902bf
DuanC001 <102814321+DuanC001@users.noreply.github.com>
2023-10-24 00:29:32 -0700

    Update script.js

--- explore-page/script.js
+++ explore-page/script.js
@@ -1,6 +1,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
-import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore.js";
+import { getFirestore, collection, getDocs, where, query } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-analytics.js";
+
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -29,6 +31,7 @@ const colRef = collection(db, 'posts')
 let events = []
 
 
+
 getDocs(colRef)
   .then((snapshot) => {
     snapshot.docs.forEach(doc => {
@@ -38,7 +41,17 @@ getDocs(colRef)
     let output = document.getElementById('eventDetails');
     events.forEach(event => {      
       const div = document.createElement('div');
-      div.innerHTML = `<br> Date: ${String(event.date)} <br> Description: ${event.description} <br> Email: ${event.email} <br> Location: ${event.location} <br> Phone Number:${event.phoneNumber} <br> Position Title: ${event.positionTitle}<br> Post Expiry Date: ${event.expireDate} <br> Preferred Language: ${event.preferredLanguage}<br> Skills: ${event.skills}<br>  Hours: ${event.hours} <br>`; 
+      div.innerHTML = `Position Title: ${event.positionTitle} 
+                      <br> Description: ${event.description} 
+                      <br> Date: ${String(event.date)}
+                      <br> Location: ${event.location} 
+                      <br> Post Expiry Date: ${event.expireDate} 
+                      <br> Intererts: ${event.interests}
+                      <br> Skills: ${event.skills}
+                      <br> Preferred Language: ${event.preferredLanguage}
+                      <br> Phone Number:${event.phoneNumber} 
+                      <br> Email: ${event.email} 
+                      <br> Hours: ${event.hours}`; 
 
       // Create an "Apply" button for each event
     const applyButton = document.createElement('button');
@@ -56,61 +69,74 @@ getDocs(colRef)
     console.log(err.message)
   })
 
-const search = document.getElementById('search')
-// Search by keywords, interests, skills or combine them.
-  search.addEventListener("click", (e)=>{
-    e.preventDefault();
-    const keyWord = document.getElementById('searchInput').value.toLowerCase()
-    const interests = document.getElementById('interests').value
-    const skills = document.getElementById('skills').value
-    let q;
-    if(interests != "Interests"){
-      if (skills != "Skills") {
-        q = query(colRef, where("skills", "==", skills), where("interests", "==", interests))
-        console.log("1")
-      } else {
-        q = query(colRef, where("interests", "==", interests))
-        console.log("2")
-      }
-    }else{
-      if (skills != "Skills") {
-        q = query(colRef, where("skills", "==", skills))
-        console.log("3")
-      } else {
-        q = colRef
-        console.log("4")
-      }
+const search = document.getElementById('search');
+const output = document.getElementById('eventDetails');
+const searchInput = document.getElementById('searchInput');
+const interestsSelect = document.getElementById('interests');
+const skillsSelect = document.getElementById('skills');
+
+search.addEventListener("click", async (e) => {
+  e.preventDefault();
+  output.innerHTML = ''; // Clear previous results
+  const keyWord = searchInput.value.toLowerCase();
+  const interests = interestsSelect.value;
+  const skills = skillsSelect.value;
+
+  // Clear input fields and reset dropdown values
+  searchInput.value = '';
+  interestsSelect.value = 'Interests'; // Reset dropdown to default
+  skillsSelect.value = 'Skills'; // Reset dropdown to default
+
+  let q;
+  if (interests != "Interests") {
+    if (skills != "Skills") {
+      q = query(colRef, where("skills", "==", skills), where("interests", "==", interests));
+    } else {
+      q = query(colRef, where("interests", "==", interests));
     }
-    
-    getDocs(q)
-    .then((snapshot) => {
+  } else {
+    if (skills != "Skills") {
+      q = query(colRef, where("skills", "==", skills));
+    } else {
+      q = colRef;
+    }
+  }
+
+  try {
+    const snapshot = await getDocs(q);
     const matchingDocs = snapshot.docs.filter((doc) => {
       return doc.data().positionTitle.toLowerCase().includes(keyWord);
     });
+
     matchingDocs.forEach(doc => {
-      events.push({ ...doc.data(), id: doc.id })
-    })
-    console.log(events)
-    let output = document.getElementById('eventDetails');
-    events.forEach(event => {      
+      const event = { ...doc.data(), id: doc.id };
       const div = document.createElement('div');
-      div.innerHTML = `<br> Date: ${String(event.date)} <br> Description: ${event.description} <br> Email: ${event.email} <br> Location: ${event.location} <br> Phone Number:${event.phoneNumber} <br> Position Title: ${event.positionTitle}<br> Post Expiry Date: ${event.expireDate} <br> Preferred Language: ${event.preferredLanguage}<br> Skills: ${event.skills}<br> Intererts: ${event.interests}<br> Hours: ${event.hours} <br>`; 
+      div.innerHTML = `Position Title: ${event.positionTitle} 
+      <br> Description: ${event.description} 
+      <br> Date: ${String(event.date)}
+      <br> Location: ${event.location} 
+      <br> Post Expiry Date: ${event.expireDate} 
+      <br> Intererts: ${event.interests}
+      <br> Skills: ${event.skills}
+      <br> Preferred Language: ${event.preferredLanguage}
+      <br> Phone Number:${event.phoneNumber} 
+      <br> Email: ${event.email} 
+      <br> Hours: ${event.hours}`;
+      
+      const applyButton = document.createElement('button');
+      applyButton.textContent = 'Apply';
 
-      // Create an "Apply" button for each event
-    const applyButton = document.createElement('button');
-    applyButton.textContent = 'Apply';
+      applyButton.addEventListener('click', () => {
+        console.log(`Clicked on Apply for event with description: ${event.description}`);
+        alert("Applied for the opportunity");
+      });
 
-    applyButton.addEventListener('click', () => {
-      console.log(`Clicked on Apply for event with description: ${event.description}`);
-      alert("Applied for the opportunity")
-    });
       output.appendChild(div);
       output.appendChild(applyButton);
     });
-  })
-  .catch(err => {
-    console.log(err.message)
-  })
-   })
+  } catch (err) {
+    console.log(err.message);
+  }
+});
 
  
