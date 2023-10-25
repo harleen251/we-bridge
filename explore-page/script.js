86114fbf3cb2c9e52e59e70e38f493113b828494
harleen <h251@mylangara.ca>
2023-10-24 19:24:00 -0700

    enable interest and skill filter

--- explore-page/script.js
+++ explore-page/script.js
@@ -1,13 +1,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
-import { getFirestore, collection, getDocs, where, query } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore.js";
+import { getFirestore, collection, getDocs, onSnapshot, where, query } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-analytics.js";
 
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyBiW_sL8eKxcQ7T9xKqQJxxRaIHmizOBoE",
     authDomain: "webridge-81f09.firebaseapp.com",
@@ -28,46 +23,22 @@ const db = getFirestore();
 
 const colRef = collection(db, 'posts')
 
-let events = []
+let events = [];
+let filteredPosts = [];
 
-
-
+//getting recommendations
 getDocs(colRef)
   .then((snapshot) => {
     snapshot.docs.forEach(doc => {
       events.push({ ...doc.data(), id: doc.id })
     })
-    console.log(events)
-    let output = document.getElementById('eventDetails');
-    events.forEach(event => {      
-      const div = document.createElement('div');
-      div.innerHTML = `Position Title: ${event.positionTitle} 
-                      <br> Description: ${event.description} 
-                      <br> Date: ${String(event.date)}
-                      <br> Location: ${event.location} 
-                      <br> Post Expiry Date: ${event.expireDate} 
-                      <br> Intererts: ${event.interests}
-                      <br> Skills: ${event.skills}
-                      <br> Preferred Language: ${event.preferredLanguage}
-                      <br> Phone Number:${event.phoneNumber} 
-                      <br> Email: ${event.email} 
-                      <br> Hours: ${event.hours}`; 
-
-      // Create an "Apply" button for each event
-    const applyButton = document.createElement('button');
-    applyButton.textContent = 'Apply';
-
-    applyButton.addEventListener('click', () => {
-      console.log(`Clicked on Apply for event with description: ${event.description}`);
-      alert("Applied for the opportunity")
-    });
-      output.appendChild(div);
-      output.appendChild(applyButton);
-    });
+    displayResult(events);
   })
   .catch(err => {
     console.log(err.message)
   })
+
+
 
 const search = document.getElementById('search');
 const output = document.getElementById('eventDetails');
@@ -130,13 +101,63 @@ search.addEventListener("click", async (e) => {
         console.log(`Clicked on Apply for event with description: ${event.description}`);
         alert("Applied for the opportunity");
       });
-
+      div.appendChild(applyButton);
       output.appendChild(div);
-      output.appendChild(applyButton);
     });
   } catch (err) {
     console.log(err.message);
   }
 });
+
+let selectSkillsID = document.getElementById('skills');
+selectSkillsID.addEventListener('change', function(e){
+  e.preventDefault();
+  let selectedSkillToFilter = selectSkillsID.options[selectSkillsID.selectedIndex];
+  fetchData("skills", selectedSkillToFilter.textContent);
+})
+
+let selectInterestsID = document.getElementById('interests');
+selectInterestsID.addEventListener('change', function(e){
+  e.preventDefault();
+  let selectedInterestToFilter = selectInterestsID.options[selectInterestsID.selectedIndex];
+  fetchData("interests", selectedInterestToFilter.textContent);
+})
+
+function fetchData(field, value){
+
+  filteredPosts = [];
+
+  const q = query(colRef, where(field, "==", value));
+  onSnapshot(q, (snapshot) => {
+    snapshot.docs.forEach((element) => {
+      filteredPosts.push({ ...element.data(), id: element.id })
+      displayResult(filteredPosts);
+      console.log("my array", filteredPosts);
+
+  });
+})
+}
+
+
+function displayResult(events){
+  let output = document.getElementById('eventDetails');
+    output.innerHTML = " ";
+    events.forEach(event => {      
+      const div = document.createElement('div');
+      div.innerHTML = `<br> Date: ${String(event.date)} <br> Description: ${event.description} <br> Email: ${event.email} <br> Location: ${event.location} <br> Phone Number:${event.phoneNumber} <br> Position Title: ${event.positionTitle}<br> Post Expiry Date: ${event.expireDate} <br> Preferred Language: ${event.preferredLanguage}<br> Skills: ${event.skills}<br>  Hours: ${event.hours} <br>`; 
+
+    // Create an "Apply" button for each event
+    const applyButton = document.createElement('button');
+    applyButton.id = 'applyButton'
+    applyButton.textContent = 'Apply';
+
+    applyButton.addEventListener('click', () => {
+      console.log(`Clicked on Apply for event with description: ${event.description}`);
+      alert("Applied for the opportunity")
+    });
+      div.appendChild(applyButton);
+      output.appendChild(div);
+    });
+}
 
  
