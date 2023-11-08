1e11f7c4de3761b13203693e88e25c355e4f33ba
greeshma017 <gdharmapalan00@mylangara.ca>
2023-11-08 10:05:09 -0800

    update on org account

--- js/organization_account.js
+++ js/organization_account.js
@@ -75,7 +75,7 @@ async function handleManageButtonEvent(event) {
 const postsRef = collection(db, 'posts');
 
 async function getPostList() {
-    const q = query(postsRef, where( "organizationID", "==" , idOrganization ), orderBy("date", "desc"));
+    const q = query(postsRef, where( "organizationId", "==" , idOrganization ), orderBy("date", "desc"));
     const postFilter = document.getElementById("postFilter").value;
 
     const currentDate = new Date();
@@ -210,7 +210,7 @@ async function getApplicationList() {
     const applicationFilter = document.getElementById("applicationFilter").value;
 
     document.getElementById("application-list").innerHTML = "";
-    const q2 = query(collectionRef, where( "organizationID", "==" , idOrganization ), orderBy("dateApplied", "desc"));
+    const q2 = query(collectionRef, where( "organizationId", "==" , idOrganization ), orderBy("dateApplied", "desc"));
 
     let applicationArray = [];
     await getDocs(q2, collectionRef)
