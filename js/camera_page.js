8502b7660acaf07b3d579864c2c248c1132c9085
harleen <h251@mylangara.ca>
2023-11-14 21:22:45 -0800

    fix qr code validation

--- js/camera_page.js
+++ js/camera_page.js
@@ -62,7 +62,7 @@ const scanner = new Html5QrcodeScanner('reader', {
     },
     fps: 20,
     });
-    scanner.render(successForCheckin, error);
+     await scanner.render(successForCheckin, error);
     
     function error(err) {
     console.error(err);
@@ -75,69 +75,140 @@ const scanner = new Html5QrcodeScanner('reader', {
     let checkInStatusValue = await getTheCookieForCheckInCheckOut();
     console.log("checkInStatusValue", checkInStatusValue);
 
-     async function successForCheckin() {
+    async function extractPostId(decodedText) {
+        console.log(decodedText);
+    const checkInSuccessfulIndex = decodedText.indexOf("Check-In Successful");
+
+    if (checkInSuccessfulIndex !== -1) {
+        const postIdMatch = decodedText.match(/Post:\s*([^\s]+)/);
+
+        if (postIdMatch && postIdMatch[1]) {
+            const postId = postIdMatch[1];
+            return { checkInSuccessful: true, postId: postId };
+        } else {
+            console.error("Post ID not found in the decoded text.");
+            return { checkInSuccessful: false, postId: null };
+        }
+    } else {
+        console.error("Check-In Successful not found in the decoded text.");
+        return { checkInSuccessful: false, postId: null };
+    }
+
+    }
+
+    async function extractPostIdForCheckout(decodedText){
+    const workCompletedIndex = decodedText.indexOf("Work Completed");
+
+    if (workCompletedIndex !== -1) {
+        const postIdMatch = decodedText.match(/Post:\s*([^\s]+)/);
+
+        if (postIdMatch && postIdMatch[1]) {
+            const postId = postIdMatch[1];
+            return { workCompleted: true, postId: postId };
+        } else {
+            console.error("Post ID not found in the decoded text.");
+            return { workCompleted: false, postId: null };
+        }
+    } else {
+        console.error("Work Completed not found in the decoded text.");
+        return { workCompleted: false, postId: null };
+    }
+    }
+    const applicationRef = collection( db, 'application');
+
+
+
+
+
+     async function successForCheckin(decodedText) {
+        console.log("decoded text is",decodedText);
         
-        if (checkInStatusValue == "true") {
+        let applicationStatus = " ";
 
-            var checkInDate = new Date();
+        const q = query(applicationRef, where("volunteerID", "==", volunteerIdFromCookie), where("postsID", "==", postIdFromCookie));
+        await getDocs(q, applicationRef)
+            .then((snapshot) => {
+                snapshot.forEach((document) => {
+                    applicationStatus = document.data().status;
+                })
+            })
+        if (applicationStatus === "approved") {
+            if (checkInStatusValue == "true") {
+                const { checkInSuccessful, postId } = await extractPostId(decodedText);
+                if (checkInSuccessful) {
+                var checkInDate = new Date();
 
-
-            try {
-                const colRef = collection(db, 'volunteerRecord');
-
-                await addDoc(colRef, {
-                    // Store the scanned QR code data
-                    checkInDate: checkInDate, 
-                    // checkOutDate: " ",
-                    // hours: " ",
-                    volunteerID: volunteerIdFromCookie,
-                    postsID: postIdFromCookie
-                });
-        
-
-                window.location.href = "volunteer_account.html#commitment";
-                // submitPost.reset();
-            } catch (error) {
-                console.error("Error adding document: ", error);
-                alert("An error occurred while submitting the post.");
+                try {
+                    const colRef = collection(db, 'volunteerRecord');
+    
+                    await addDoc(colRef, {
+                        // Store the scanned QR code data
+                        checkInDate: checkInDate, 
+                        volunteerID: volunteerIdFromCookie,
+                        postsID: postIdFromCookie
+                    });
+            
+    
+                    window.location.href = "volunteer_account.html#commitment";
+                    // submitPost.reset();
+                } catch (error) {
+                    console.error("Error adding document: ", error);
+                    alert("An error occurred while submitting the post.");
+                }
+            }
+            else if (!checkInSuccessful) {
+                alert('Invalid QR code');
             }
         }
-        else {
-            const dataRef = collection(db, 'volunteerRecord');
-            const q = query(dataRef, where("volunteerID", "==", volunteerIdFromCookie), where("postsID", "==", postIdFromCookie));
-            
-            await getDocs(q,dataRef)
-                .then((querySnapshot) => {
-                    querySnapshot.forEach((document) => {
-                    console.log("document", document);
-                    const documentRef = doc(dataRef, document.id);
-                    console.log("document.id", document.id);
-                    let checkInTime = document.data().checkInDate.seconds;
-                    console.log("checkintime", checkInTime);
-                    let checkOutTime = new Date();
-                    console.log("checkouttime", checkOutTime);
+            else {
+                const { workCompleted, postId } = await extractPostIdForCheckout(decodedText);
+                if (workCompleted) {
+                await updateStatusInApplicationCollection();
+                    
+                const dataRef = collection(db, 'volunteerRecord');
+                const q = query(dataRef, where("volunteerID", "==", volunteerIdFromCookie), where("postsID", "==", postIdFromCookie));
+                
+                await getDocs(q,dataRef)
+                    .then((querySnapshot) => {
+                        querySnapshot.forEach((document) => {
+                        console.log("document", document);
+                        const documentRef = doc(dataRef, document.id);
+                        console.log("document.id", document.id);
+                        let checkInTime = document.data().checkInDate.seconds;
+                        console.log("checkintime", checkInTime);
+                        let checkOutTime = new Date();
+                        console.log("checkouttime", checkOutTime);
+    
+                        const dataToUpdate = {
+                            checkOutDate: checkOutTime,
+                            hours: calculateHours(checkInTime, checkOutTime)
+                        };
+    
+                         updateDoc(documentRef, dataToUpdate)
+                            .then(() => {
+                            console.log('Document field updated successfully.');
+                            })
+                            .then(() =>{
+                            window.location.href = "volunteer_record.html";
 
-                    const dataToUpdate = {
-                        checkOutDate: checkOutTime,
-                        hours: calculateHours(checkInTime, checkOutTime)
-                    };
-
-                     updateDoc(documentRef, dataToUpdate)
-                        .then(() => {
-                        console.log('Document field updated successfully.');
-                        window.location.href = "volunteer_account.html#commitment";
-                        })
-                        .catch((error) => {
-                        console.error('Error updating document field:', error);
-                        alert("An error occurred while submitting the post.");
+                            })
+                            .catch((error) => {
+                            console.error('Error updating document field:', error);
+                            alert("An error occurred while submitting the post.");
+                            });
                         });
+                    })
+                    .catch((error) => {
+                        console.error("Error querying for documents:", error);
                     });
-                })
-                .catch((error) => {
-                    console.error("Error querying for documents:", error);
-                });
-
+                }
+                else {
+                    alert('Invalid QR Code');
+                }
+    
+            }
         }
+        
             
             scanner.clear();
             document.getElementById('reader').remove();
@@ -150,6 +221,32 @@ const scanner = new Html5QrcodeScanner('reader', {
             console.log(parseFloat(roundedHoursDifference));
             return parseFloat(roundedHoursDifference);
 
+        }
+
+        async function updateStatusInApplicationCollection(){
+            const q = query(applicationRef, where( "volunteerID", "==" , volunteerIdFromCookie ), where( "postsID", "==" , postIdFromCookie ));
+
+                    await getDocs(q,applicationRef)
+                    .then((querySnapshot) => {
+                        querySnapshot.forEach((document) => {
+                        const documentRef = doc(applicationRef, document.id);
+                
+                        const dataToUpdate = {
+                            status: "complete"
+                        };
+                
+                        updateDoc(documentRef, dataToUpdate)
+                            .then(() => {
+                            console.log('Document field status updated to complete successfully.');
+                            })
+                            .catch((error) => {
+                            console.error('Error updating document field status:', error);
+                            });
+                        });
+                    })
+                    .catch((error) => {
+                        console.error("Error querying for documents for application collection:", error);
+                    });
         }
         
     
